baddd13d72b0c61a16ed540b3bc132836fbf01bd
Sayantan Das <sayantan2799@gmail.com>
2026-04-25 10:26:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import {Button} from '@heroui/react'
+
+function App() {
+    return (
+        <div>
+            <h1 className='text-6xl'>Hello</h1>
+            <Button>Get Started</Button>
+        </div>
+    )
+}
+
+export default App
